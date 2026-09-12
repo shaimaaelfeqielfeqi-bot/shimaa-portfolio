5ff8911c50d7291238a79f6e4da9fbe515e405f3
Shimaa Elsayed <shaimaaelfeqielfeqi@gmail.com>
2026-09-12 17:23:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Scroll Animation using Intersection Observer
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.15
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, observerOptions);
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
